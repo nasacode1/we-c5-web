56d962b251abba102c7f3d634dc2dcc38e2421a7
Maanasa <maanasarare@gmail.com>
2024-12-16 23:55:15 +0530

    Thought of what to implement in models

--- django_wordle/models.py
+++ django_wordle/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+#I must think of what tables i want for wordle. 
+# in wordle game, i have 6 attempts to find the word. The list of 5 letter words must be stored. - CLASS: words FIELDS: word_name
+# in the 6 attempts, i must store the words entered by the user. CLASS: Attempts FIELDS: word_name. 
+
+
