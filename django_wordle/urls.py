43b08ec00a333dba38a642f5c911b35469473887
Maanasa <maanasarare@gmail.com>
2024-12-17 22:51:02 +0530

    created views

--- django_wordle/urls.py
+++ django_wordle/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("grid_display/", views.grid_display, name="grid_display"),
+    path("submission/", views.submission, name="submission"),
+    path("results/", views.results, name="results"),
 ]
--- django_wordle/views.py
+++ django_wordle/views.py
@@ -7,3 +7,15 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the wordle index.")
 
+#grid display is one view. 
+# entering texts is one action that should get into action when i have typed 5 letter
+#if i found correct ans, it should display YOU WON. 
+
+def grid_display(request):
+    return HttpResponse("Youre looking at grid display")
+
+def submission(request):
+    return HttpResponse("youre submitting your guess")
+
+def results(request):
+    return HttpResponse("youre looking at results")
